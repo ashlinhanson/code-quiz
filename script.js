2d8fc1d320faca295e5daf7c7c9c4ff0ef0964f6
ashlinhanson <ashlinhanson@gmail.com>
2020-08-11 13:56:50 -0500

    wire framing functions and event listeners

--- script.js
+++ script.js
@@ -24,47 +24,95 @@ var totalScore = document.getElementById("#totalScore");
 //create an array for questions
 var quizQs = [
     {
-        question : "What does JavaScript control?",
-        options : {
+        "questionHead" : "What does JavaScript control?",
             "first" : "a. Structure",
             "second" : "b. Style",
             "third" : "c. Behavior",
             "fourth" : "d. Aesthetic",
             "rightOption" : "c. Behavior"
-        },
-   },
-   {
-        question : "What is a Boolean?",
-        options : {
+    },{
+        "questionHead" : "What is a Boolean?",
             "first" : "a. A true or false statement",
             "second" : "b. A conditional statement",
             "third" : "c. Something",
             "fourth" : "d. Something",
             "rightOption" : "a. A true or false statement"
-        },
-    },
-   {
-       question : "Who invented JavaScript?",
-       options : {
+    },{
+            "questionHead" : "Who invented JavaScript?",
             "first" : "a. Elon Musk",
             "second" : "b. Mitchell Baker",
             "third" : "c. Bill Gates",
             "fourth" : "d. Brenden Eich",
             "rightOption" : "d. Brenden Eich"
-      },
-   },
-   {
-    question : "Something about something?",
-    options : {
+      },{
+            "questionHead" : "Something about something?",
             "first" : "a. something",
             "second" : "b. something",
             "third" : "c. something",
             "fourth" : "d. something",
-            "rightOption" : ""
-   },
-   },
+            "rightOption" : "something"
+        }
 ];
 
 
 //function to keep score during the quiz
-var score = 0; //this holds data for the score numbers 
+var score = 0; 
+
+
+
+// function to display the introduction page
+function introductionPage() {
+    introPage.style.display = "block";
+    introHead.style.display = "block";
+    questionsPage.style.display = "none";
+    questionHead.style.display = "none";
+    resultsPage.style.display = "none";
+    resultsHead.style.display = "none";
+}
+
+//function to start the quiz
+function quizStart() {
+    questionsPage.style.display = "block";
+    questionHead.style.display = "block";
+}
+//show questions
+function questionDisplay() {
+  
+
+//check user input for the right option
+function checkOption(event) {
+    
+}
+
+//results page
+
+
+
+//input initials
+
+
+//scoreboard
+
+
+//EVENT LISTENERS 
+function questionDisplay(){
+    optionA.addEventListener("click", function (event){
+        checkOption(event);
+    })
+    optionB.addEventListener("click", function (event){
+        checkOption(event);
+    })
+    optionC.addEventListener("click", function (event){
+        checkOption(event);
+    })
+    optionD.addEventListener("click", function (event){
+        checkOption(event);
+    })
+}
+
+function quizStart(){
+    startButton.addEventListener("click", function(){
+        quizStart()
+        console.log("start");
+    })
+}
